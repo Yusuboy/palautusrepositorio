eb3723c6e5df0f0f803be5cb6b87447621130a34
Yusuboy <Yuusufab@outlook.com>
2023-11-11 22:55:22 +0200

    Muodostettiin projecti-olio ja tulostuson oikea

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -7,16 +7,29 @@ class ProjectReader:
     def __init__(self, url):
         self._url = url
 
+
     def get_project(self):
         # Tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print("raaka TOML tiedosto:")
+        print("Raaka TOML tiedosto")
+        print()
         print(content)
 
         # Käytä toml.loads muuntaaksesi TOML-merkkijono Pythonin tietorakenteeksi
-        project_data = toml.loads(content)
-        print("\muokattu python tietorakenteeksi:")
-        print(project_data)
+        print("Muutetaan python tietorakenteeksi")
+        toml_data = toml.loads(content)
+        print()
+        print(toml_data)
 
-        # Palauta käsitelty projektitieto
-        return project_data
+        # Luo Project-olio annetuista tiedoista
+        project = Project(
+            name= toml_data.get("tool", {}).get("poetry", {}).get("name", ""),
+            description= toml_data.get("tool", {}).get("poetry", {}).get("description", ""),
+            dependencies= toml_data.get("tool", {}).get("poetry", {}).get("dependencies", []),
+            dev_dependencies= toml_data.get("tool", {}).get("poetry", {}).get("group", {}).get("dev", {}).get("dependencies", []),
+        )
+
+        # Tulosta projekti
+        print()
+        print("Tulostetaan muokattu tietorakenne")
+        return project
