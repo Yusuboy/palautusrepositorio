b2997fc0ffbe9c44e7f64ef880b03c6e86802a5a
Yusuboy <Yuusufab@outlook.com>
2023-12-11 20:27:41 +0200

    4 ja 5 done

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,24 +1,34 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, All, Or
+from matchers import And, HasAtLeast, PlaysIn, Not, All, Or, QueryBuilder
 
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+    url = "https://studies.cs.helsinki.fi//nhlstats/2022-23/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("NYR"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
-        )
+    query = QueryBuilder()
+
+    m1 = (
+    query
+        .playsIn("PHI")
+        .hasAtLeast(10, "assists")
+        .hasFewerThan(5, "goals")
+        .build()
     )
+
+    m2 = (
+    query
+        .playsIn("EDM")
+        .hasAtLeast(50, "points")
+        .build()
+    )
+
+    matcher = query.oneOf(m1, m2).build()
+
     for player in stats.matches(matcher):
         print(player)
-
 
 
 
